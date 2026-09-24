59e2d87f2e593a1485ba24221bcbc6d8dfdb307e
98731 <105843577+teremuhamblin@users.noreply.github.com>
2026-09-24 11:03:07 +0200

    Create module_name.sh

---
+++ modules/templates/module_template/module_name.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# ============================================================
+# MODULE : module_name
+# ROLE   : Fonction tactique pour CarFactory
+# MODE   : Compatible moteur v2.x
+# ============================================================
+
+MODULE_NAME="module_name"
+
+log() {
+    echo "[${MODULE_NAME}] $1"
+}
+
+init() {
+    log "Initialisation du module"
+}
+
+run() {
+    log "Execution du module"
+}
+
+case "$1" in
+    init) init ;;
+    run)  run ;;
+    *)    log "Commande inconnue" ;;
+esac
